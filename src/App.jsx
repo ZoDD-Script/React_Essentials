bacb080089521da89cb5a74a8fa1b10f7dd4fd0b
Chukwu Blessed <blessedchukwu89@gmail.com>
2024-06-13 15:04:10 +0100

    cleaning code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
 import Header from './components/Header/Header.jsx'
-import CoreConcept from './components/CoreConcept.jsx'
-import TabButton from './components/TabButton.jsx'
-import { CORE_CONCEPTS, EXAMPLES } from './data.js'
 import CoreConcepts from './components/CoreConcepts.jsx'
 import Examples from './components/Examples.jsx'
 
